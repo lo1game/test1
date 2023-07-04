98323b7cb97629e8eb92b8f928d90d867d675a4a
lo1game <ducthien856@gmail.com>
2023-07-04 19:12:20 +0700

    testcode

--- src/testcode/Testcode.java
+++ src/testcode/Testcode.java
@@ -17,6 +17,7 @@ public class Testcode {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("welcomehome");
+        System.out.println("chanvl");
     }
     
 }
